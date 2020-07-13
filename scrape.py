e56ff895061415ede5529864a84dfaf9845f6db4
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-07-13 13:19:13 -0400

    create a function called create-custon_hn that holds the links and votes. Created an array for hn and combine the votes, title, and links together.

--- scrape.py
+++ scrape.py
@@ -8,4 +8,14 @@ soup = BeautifulSoup(res.text,'html.parser')
 links = soup.select('.storylink')
 votes = soup.select('.score')
 
-print(votes[0])
+def create_custom_hn(links,votes):
+    hn =[]
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        points = int(votes[idx].getText().replace('points', ''))
+        print(points)
+        hn.append({'title': title, 'link': href})
+
+    return hn
+print(create_custom_hn(links,votes))
